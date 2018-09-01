3ef25bbc98ec9ccbac2adbe0c05e78a586354e70
Joker <303359872@qq.com>
2018-09-01 12:02:02 +0800

    002 Debug

--- Tower Defense/Assets/Scripts/Turret.cs
+++ Tower Defense/Assets/Scripts/Turret.cs
@@ -34,30 +34,27 @@ public class Turret : MonoBehaviour {
     private void Update()
     {
         timer += Time.deltaTime;
-        print(enemys.Count);
-        if (enemys.Count > 0 && enemys[0] != null)
-        {
+       // print(enemys.Count);
+        
+        if (enemys.Count > 0 && enemys[0] == null) UpdateList();
+        if (enemys.Count <= 0) { timer = attackRate; return; }
+
+
             Vector3 targetPos = enemys[0].transform.position;
             targetPos.y = head.position.y;
             head.LookAt(targetPos);
-        }
-
-        if (enemys.Count>0 && timer > attackRate && enemys[0] != null)
-        {
-            timer = 0;
-            Attack();
-        }
+            if (timer > attackRate)
+            {
+                timer = 0;
+                Attack();
+            }
+        
+    }
 
       //  if (enemys[0] == null) enemys.Remove(enemys[0]);   第一个被销毁的不一定是数组第一个，（除了到达终点死意外，还可以被其他炮塔打死）
-    }
+    
     void Attack()
     {
-        if (enemys[0] == null)
-        {
-            //enemys.RemoveAll(null);
-            UpdateList();
-            if (enemys.Count <= 0) { timer = attackRate; return; }
-        }
         GameObject bullet= GameObject.Instantiate(weaponPrefab, firePosition.position, firePosition.rotation);
         bullet.GetComponent<Bullet>().SetTarget(enemys[0].transform);
     }
@@ -67,12 +64,11 @@ public class Turret : MonoBehaviour {
       //  List<int> emptyIndex = new List<int>();
 
         for (int i=0; i<enemys.Count; i++)
-        {
             if  (enemys[i] == null)
             {
                 enemys.RemoveAt(i);
                 i--;
             }
-        }
+        
     }
 }
